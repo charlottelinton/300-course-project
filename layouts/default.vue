b18aeda03953901452a987c8ed6b808f09e2dea0
calinton <charlottelinton@users.noreply.github.com>
2019-06-06 19:52:33 -0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <AppNav/>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+// Importing nav for use across all pages
+import AppNav from '../components/AppNav';
+
+export default {
+  components: {
+    AppNav,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  router: {
+    base: '/html300-9-assignment/'
+   },
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Limelight|Merriweather' }
+    ],
+    script:[
+            {src:'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'},
+            {src:'https://code.jquery.com/jquery-3.3.1.slim.min.js'}
+        ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt',
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
